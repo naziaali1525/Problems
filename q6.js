0c62e28ad75efabab40cf957f91ecb20cc458d3b
naziaali1525 <naziaali688@gmail.com>
2018-06-06 10:59:37 -0700

    new

---
+++ q6.js
@@ -0,0 +1,8 @@
+let highScore = 100;
+let securedScore = 18;
+
+if (securedScore > highScore) {
+    console.log("Congratulations!!! Your new high score is 118");
+} else {
+    console.log("Try Again. you just need more points to win");
+}
